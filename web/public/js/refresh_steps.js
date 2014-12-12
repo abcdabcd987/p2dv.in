b3c0680a18546001d349a3acb7fae6b2911101f8
Lequn Chen <chenlequn22@gmail.com>
2014-12-12 22:46:08 +0800

    Battle List page AJAX

--- web/public/js/refresh_steps.js
+++ web/public/js/refresh_steps.js
@@ -1,4 +1,5 @@
-function refresh_steps(url) {
+// Demo page
+function refresh_step(url) {
     var span_status = $("#span-status");
     var span_steps = $("#span-steps");
     var dd_steps = $("#dd-steps");
@@ -14,9 +15,33 @@ function refresh_steps(url) {
         } else if (data.status === 'Finished') {
             span_status.attr('class', 'label label-success');
             span_status.html(data.status);
+            span_steps.html(data.step);
             window.location.reload();
             return;
         }
-        setTimeout('refresh_steps("'+url+'")', 50);
+        setTimeout(refresh_step, 50, url);
     });
+};
+
+// List page
+function watch(trID, url) {
+    var tr = $(trID);
+    var span_status = tr.find('.status').eq(0);
+    var td_steps = tr.find('.steps').eq(0);
+    $.getJSON(url, function(data) {
+        if (data.status === 'Running') {
+            if (span_status.html() === 'Pending') {
+                span_status.attr('class', 'label label-warning');
+                span_status.html(data.status);
+            }
+            td_steps.html(data.step);
+        } else if (data.status === 'Finished') {
+            span_status.attr('class', 'label label-success');
+            span_status.html(data.status);
+            td_steps.html(data.step);
+            window.location.reload();
+            return;
+        }
+        setTimeout(watch, 50, trID, url);
+    })
 }
