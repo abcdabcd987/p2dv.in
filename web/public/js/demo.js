cce6e6da85d7d2852ca7761010b71b2e464f71b9
BreakVoid <breakvoid@hotmail.com>
2015-11-23 21:39:58 +0800

    save changes

--- web/public/js/demo.js
+++ web/public/js/demo.js
@@ -9,6 +9,7 @@ Demo = {
     // DOM:
     board     : undefined,
     floatChess: undefined,
+    hideChess : undefined,
     data      : undefined,
     demoText  : undefined,
     btnNext   : undefined,
@@ -18,17 +19,48 @@ Demo = {
     spanPlay  : undefined,
     spanPrev  : undefined,
 
+    //Runtime data:
+    chessData : undefined,
+    optList : undefined,
+
     // Functions:
     getBox: function getBox(i, j) {
         return Demo.board.find('#box-' + i + '-' + j);
     },
 
     drawTable: function drawTable() {
+        chessData = [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+        ];
+        optList = [[], []];
+        for (var i = 0; i < 4; ++i) {
+            for (var j = 0; j < 8; ++j) {
+                chessData[i][j] = data.initChess[i][j]; 
+            }
+        }
+        for (var i = 0; i < data.step.length; ++i) {
+            var x = data.step.source[0];
+            var y = data.step.source[1];
+            var xx = data.step.target[0];
+            var yy = data.step.target[1];
+            if (xx == x && yy == y) {
+                optList[0].push([x, y, chessData[x][y]]);
+                optList[1].push([xx, yy, chessData[xx][yy]]);
+            } else {
+                optList[0].push([x, y, chessData[x][y]]);
+                optList[1].push([xx, yy, chessData[xx][yy]]);
+                chessData[xx][yy] = chessData[x][y];
+                chessData[x][y] = 0;
+            }
+        }
         var table = $('<table id="chess-board">');
         table.append('<tr><td colspan="8" class="ai1 td-ai-name">AI2</td></tr>');
-        for (var i = 8; i >= 0; --i) {
+        for (var i = 0; i < 4; ++i) {
             var tr = $('<tr id="row-' + i + '">');
-            for (var j = 0; j < 7; ++j) {
+            for (var j = 0; j < 8; ++j) {
                 var td = $('<td id="box-' + i + '-' + j + '">');
                 td.data('row', i);
                 td.data('col', j);
@@ -42,6 +74,13 @@ Demo = {
     },
 
     putChess: function putChess() {
+
+        for (var i = 0; i < 4; ++i) {
+            for (var j = 0; j < 8; ++j) {
+                Demo.getBox(i, j).addClass('img-hide');
+            }
+        }
+/*
         Demo.getBox(0, 0).addClass('ai0').addClass('img-5');
         Demo.getBox(0, 2).addClass('ai0').addClass('img-trap').data('default-css', 'ai0 img-trap');
         Demo.getBox(0, 3).addClass('ai0').addClass('img-cave');
@@ -80,29 +119,37 @@ Demo = {
         Demo.getBox(3, 5).addClass('river').data('default-css', 'river');
         Demo.getBox(4, 5).addClass('river').data('default-css', 'river');
         Demo.getBox(5, 5).addClass('river').data('default-css', 'river');
+*/
+    },
+    flipChess: function flipChess(x, y) {
+        Demo.getBox(x, y)
     },
 
     moveChess: function moveChess(sx, sy, tx, ty) {
-        var source = Demo.getBox(sx, sy);
-        var target = Demo.getBox(tx, ty);
-        Demo.floatChess.attr('class', source.attr('class'));
-        Demo.floatChess.offset(source.offset());
-
-        if (source.data('default-css')) {
-            source.attr('class', source.data('default-css'));
+        if (sx == tx && sy == ty) {
+            target.attr('class', Demo.)
         } else {
-            source.attr('class', '');
-        }
+            var source = Demo.getBox(sx, sy);
+            var target = Demo.getBox(tx, ty);
+            Demo.floatChess.attr('class', source.attr('class'));
+            Demo.floatChess.offset(source.offset());
 
-        Demo.floatChess.animate(target.offset(), Demo.interval, function() {
-            target.attr('class', Demo.floatChess.attr('class'));
-            Demo.floatChess.addClass('hidden');
-            Demo.setControls();
-            if (!Demo.isPause) {
-                ++Demo.playing;
-                Demo.timeoutID = setTimeout(Demo.draw, Demo.interval);
+            if (source.data('default-css')) {
+                source.attr('class', source.data('default-css'));
+            } else {
+                source.attr('class', '');
             }
-        });
+
+            Demo.floatChess.animate(target.offset(), Demo.interval, function() {
+                target.attr('class', Demo.floatChess.attr('class'));
+                Demo.floatChess.addClass('hidden');
+                Demo.setControls();
+                if (!Demo.isPause) {
+                    ++Demo.playing;
+                    Demo.timeoutID = setTimeout(Demo.draw, Demo.interval);
+                }
+            });
+        }
     },
 
     setupInvalidList: function setupInvalidList() {
